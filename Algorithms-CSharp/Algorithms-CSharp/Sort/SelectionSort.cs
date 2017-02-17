dcf2e5dced31eef82ba7df3619576c254a9123f2
Matt <matt.paauw@gmail.com>
2017-02-17 11:54:42 -0800

    Refactored SelectionSort class to support generic types.

--- Algorithms-CSharp/Algorithms-CSharp/Sort/SelectionSort.cs
+++ Algorithms-CSharp/Algorithms-CSharp/Sort/SelectionSort.cs
@@ -10,25 +10,25 @@ namespace Algorithms_CSharp.Sort
     /// Class including code to perform a Selection Sort.
     /// Efficiency: O(n^2)
     /// </summary>
-    class SelectionSort
+    class SelectionSort<T>
     {
         /// <summary>
-        /// Takes an unsorted integer array and performs a Selection Sort on it.
+        /// Takes an unsorted array and performs a Selection Sort on it.
         /// </summary>
-        /// <param name="input">Unsorted int array to be sorted.</param>
-        public void sort(int[] input)
+        /// <param name="input">Unsorted array to be processed within the sort.</param>
+        public void sort(IComparable[] input)
         {
             for(int i = 0; i < input.Length; i++)
             {
                 int minIndex = i;
                 for(int j = i + 1; j < input.Length; j++)
                 {
-                    if(input[j] < input[i])
+                    if(input[j].CompareTo(input[i]) < 0)
                     {
                         minIndex = j;
                     }
                 }
-                int temp = input[i];
+                IComparable temp = input[i];
                 input[i] = input[minIndex];
                 input[minIndex] = temp;
             }
