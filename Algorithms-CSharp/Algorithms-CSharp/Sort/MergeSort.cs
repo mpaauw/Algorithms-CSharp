249dae1b35b9736a55671d7002d124f5b18f1de8
Matt <matt.paauw@gmail.com>
2017-03-01 21:29:51 -0800

    Added comment documentation to the MergeSort class.

--- Algorithms-CSharp/Algorithms-CSharp/Sort/MergeSort.cs
+++ Algorithms-CSharp/Algorithms-CSharp/Sort/MergeSort.cs
@@ -6,36 +6,36 @@ using System.Threading.Tasks;
 
 namespace Algorithms_CSharp.Sort
 {
+    /// <summary>
+    /// Class including code to perform a Merge Sort.
+    /// Efficiency: O(n log(n))
+    /// </summary>
     public class MergeSort
     {
+        /// <summary>
+        /// Takes an unsorted array and performs a Merge Sort on it.
+        /// </summary>
+        /// <param name="input">Unsorted array to be processed within the sort.</param>
+        /// <returns>Returns the sorted integer array.</returns>
         public int[] sort(int[] input)
         {
             if(input.Length < 2)
             {
                 return input;
             }
-
             int[] arr1 = input.Take(input.Length / 2).ToArray();
             int[] arr2 = input.Skip(input.Length / 2).ToArray();
-
             arr1 = sort(arr1);
             arr2 = sort(arr2);
-
             return merge(arr1, arr2);
         }
 
-        private int[] spliceArray(int[] input, int start, int end)
-        {
-            int[] newArray = new int[(end - start) + 1];
-            int iter = 0;
-            for(int i = start; i < end - 1; i++)
-            {
-                newArray[iter] = input[i];
-                iter++;
-            }
-            return newArray;
-        }
-
+        /// <summary>
+        /// Method which takes two unsorted arrays and merges them.
+        /// </summary>
+        /// <param name="arr1"></param>
+        /// <param name="arr2"></param>
+        /// <returns></returns>
         private int[] merge(int[] arr1, int[] arr2)
         {
             List<int> list1 = arr1.ToList();
@@ -65,7 +65,6 @@ namespace Algorithms_CSharp.Sort
                 newList.Add(list2[0]);
                 list2.RemoveAt(0);
             }
-
             return newList.ToArray();
         }        
     }
