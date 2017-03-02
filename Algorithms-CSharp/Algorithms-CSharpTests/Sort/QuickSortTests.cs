7054183b0e5df9c8e0146316f9e59771bb75cdfa
Matt <matt.paauw@gmail.com>
2017-03-01 18:13:06 -0800

    Added unit tests implementations to QuickSortTests class.

--- Algorithms-CSharp/Algorithms-CSharpTests/Sort/QuickSortTests.cs
+++ Algorithms-CSharp/Algorithms-CSharpTests/Sort/QuickSortTests.cs
@@ -5,15 +5,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Algorithms_CSharp.Shared;
 
 namespace Algorithms_CSharp.Sort.Tests
 {
     [TestClass]
     public class QuickSortTests
     {
+        private const int TEST_BREADTH = 200;
+
+        private const int TEST_DEPTH = 50000;
+
+        private TestEngine testDriver;
+
+        private QuickSort quickSort;
+
+        public QuickSortTests()
+        {
+            this.testDriver = new TestEngine(TEST_BREADTH, TEST_DEPTH);
+            this.quickSort = new QuickSort();
+        }
+
         [TestMethod]
         public void sortTest()
         {
+            // act / assert
+            int[] actualSort = this.quickSort.sort(this.testDriver.elements, this.testDriver.elements[0], this.testDriver.elements[this.testDriver.elements.Length - 1]);
+            Array.Sort(this.testDriver.elements);
+            int[] expectedSort = this.testDriver.elements;
+            for(int i = 0; i < actualSort.Length; i++)
+            {
+                Assert.AreEqual(expectedSort[i], actualSort[i]);
+            }
         }
     }
 }
