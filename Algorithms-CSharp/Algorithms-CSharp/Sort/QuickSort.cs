18b99d53b3f9ffcd7bd12d735a4e07caee1f039d
Matt <matt.paauw@gmail.com>
2017-03-01 18:09:55 -0800

    Added comment documentation to QuickSort class; added a QuickSortTest unit test class.

--- Algorithms-CSharp/Algorithms-CSharp/Sort/QuickSort.cs
+++ Algorithms-CSharp/Algorithms-CSharp/Sort/QuickSort.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace Algorithms_CSharp.Sort
 {
+    /// <summary>
+    /// Class including code to perform a Quick Sort.
+    /// Efficiency: O(n^2)
+    /// Algorithm implemented using the Lomuto partition scheme.
+    /// </summary>
     public class QuickSort
     {
+        /// <summary>
+        /// Takes an unsorted array, with low and high markers, and performs a Quick Sort on it.
+        /// </summary>
+        /// <param name="input">Unsorted array to be processed within the sort.</param>
+        /// <param name="low">Int value representing low marker within array.</param>
+        /// <param name="high">Int value representing high marker within array.</param>
+        /// <returns>Returns the sorted integer array.</returns>
         public int[] sort(int[] input, int low, int high)
         {
             if(low < high)
@@ -19,6 +31,13 @@ namespace Algorithms_CSharp.Sort
             return input;
         }
 
+        /// <summary>
+        /// Algorithm used to find a pivot point within the array to be used when sorting.
+        /// </summary>
+        /// <param name="input">Unsorted array to be processed.</param>
+        /// <param name="low">Int value representing low marker within array.</param>
+        /// <param name="high">Int value representing high marker within array.</param>
+        /// <returns>Returns an int value representing a new pivot index within the array.</returns>
         private int partition(int[] input, int low, int high)
         {
             int pivot = input[high];
