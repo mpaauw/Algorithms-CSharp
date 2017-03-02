3d8db5519ee6a6906459613835bc456468534af7
Matt <matt.paauw@gmail.com>
2017-03-01 19:22:10 -0800

    Refactoring MergeSort class code.

--- Algorithms-CSharp/Algorithms-CSharp/Sort/MergeSort.cs
+++ Algorithms-CSharp/Algorithms-CSharp/Sort/MergeSort.cs
@@ -10,42 +10,97 @@ namespace Algorithms_CSharp.Sort
     {
         public int[] sort(int[] input)
         {
-            int[] inputB = input;
-            splitMerge(input, inputB, 0, input.Length);
-            return input;
-        }
-
-        private void splitMerge(int[] inputA, int[] inputB, int start, int end)
-        {
-            if(end - start < 2)
+            if(input.Length < 2)
             {
-                return;
+                return input;
             }
-            int mid = (end + start) / 2;
-            splitMerge(inputA, inputB, start, mid);
-            splitMerge(inputA, inputB, mid, end);
 
-            //return?:
-            merge(inputB, inputA, start, mid, end);
+            int[] arr1 = spliceArray(input, 0, input.Length / 2);
+            int[] arr2 = spliceArray(input, (input.Length / 2) + 1, input.Length);
+
+            arr1 = sort(arr1);
+            arr2 = sort(arr2);
+
+            return merge(arr1, arr2);
         }
 
-        private void merge(int[] inputA, int[] inputB,int start, int mid, int end)
+        private int[] spliceArray(int[] input, int start, int end)
         {
-            int i = start, j = mid;
-            for(int k = start; k < end; k++)
+            int[] newArray = new int[end - start];
+            for(int i = start; i < end; i++)
             {
-                if(i < mid && (j >= end || inputA[i] < inputA[j]))
+                newArray[i] = input[i];
+            }
+            return newArray;
+        }
+
+        private int[] merge(int[] arr1, int[] arr2)
+        {
+            int[] arr3 = new int[arr1.Length];
+            int i = 0, iter = 0;
+            for(i = 0; i < arr1.Length && i < arr2.Length; i++)
+            {
+                if(arr1[i] > arr2[i])
                 {
-                    inputB[k] = inputA[i];
-                    i = i + 1;
+                    arr3[iter] = arr2[i];
+                    iter++;
                 }
                 else
                 {
-                    inputB[k] = inputA[j];
-                    j = j + 1;
+                    arr3[iter] = arr1[i];
+                    iter++;
                 }
             }
+            for(int j = i; j < arr1.Length; j++)
+            {
+                arr3[iter] = arr1[j];
+                iter++;
+            }
+            for(int j = i; j < arr2.Length; j++)
+            {
+                arr3[iter] = arr2[j];
+                iter++;
+            }
+            return arr3;
         }
+
+        //public int[] sort(int[] input)
+        //{
+        //    int[] inputB = input;
+        //    splitMerge(input, inputB, 0, input.Length);
+        //    return input;
+        //}
+
+        //private void splitMerge(int[] inputA, int[] inputB, int start, int end)
+        //{
+        //    if(end - start < 2)
+        //    {
+        //        return;
+        //    }
+        //    int mid = (end + start) / 2;
+        //    splitMerge(inputA, inputB, start, mid);
+        //    splitMerge(inputA, inputB, mid, end);
+
+        //    merge(inputB, inputA, start, mid, end);
+        //}
+
+        //private void merge(int[] inputA, int[] inputB,int start, int mid, int end)
+        //{
+        //    int i = start, j = mid;
+        //    for(int k = start; k < end; k++)
+        //    {
+        //        if(i < mid && (j >= end || inputA[i] < inputA[j]))
+        //        {
+        //            inputB[k] = inputA[i];
+        //            i = i + 1;
+        //        }
+        //        else
+        //        {
+        //            inputB[k] = inputA[j];
+        //            j = j + 1;
+        //        }
+        //    }
+        //}
         
     }
 }
