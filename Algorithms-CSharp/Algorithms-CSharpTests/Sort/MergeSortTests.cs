07f080b5bb2adc0448206adac7c97a3b5db8d4f1
Matt <matt.paauw@gmail.com>
2017-03-01 19:04:16 -0800

    Added unit test with implementation to MergeSortTests unit test file.

--- Algorithms-CSharp/Algorithms-CSharpTests/Sort/MergeSortTests.cs
+++ Algorithms-CSharp/Algorithms-CSharpTests/Sort/MergeSortTests.cs
@@ -5,15 +5,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Algorithms_CSharp.Shared;
 
 namespace Algorithms_CSharp.Sort.Tests
 {
     [TestClass]
     public class MergeSortTests
     {
+        private const int TEST_BREADTH = 200;
+
+        private const int TEST_DEPTH = 50000;
+
+        private TestEngine testDriver;
+
+        private MergeSort mergeSort;
+
+        public MergeSortTests()
+        {
+            this.testDriver = new TestEngine(TEST_BREADTH, TEST_DEPTH);
+            this.mergeSort = new MergeSort();
+        }
+
         [TestMethod]
         public void sortTest()
         {
+            int[] actualSort = this.mergeSort.sort(this.testDriver.elements);
+            Array.Sort(this.testDriver.elements);
+            int[] expectedSort = this.testDriver.elements;
+            for(int i = 0; i < actualSort.Length; i++)
+            {
+                Assert.AreEqual(expectedSort[i], actualSort[i]);
+            }
         }
     }
 }
