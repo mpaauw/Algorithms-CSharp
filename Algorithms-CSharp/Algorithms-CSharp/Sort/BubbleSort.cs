791e18b7f547ba3529971ba47e4487cc461dea6b
Matt <matt.paauw@gmail.com>
2017-03-02 19:03:11 -0800

    Added BubbleSort class with code implementation.

---
+++ Algorithms-CSharp/Algorithms-CSharp/Sort/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_CSharp.Sort
+{
+    public class BubbleSort
+    {
+        public int[] sort(int[] input)
+        {
+            int n = input.Length;
+            bool swapped = false;
+            do
+            {
+                for(int i = 1; i <= n - 1; i++)
+                {
+                    if(input[i - 1] > input[i])
+                    {
+                        int temp = input[i - 1];
+                        input[i - 1] = input[i];
+                        input[i] = temp;
+                        swapped = true;
+                    }
+                }
+                n -= 1;
+            } while (!swapped);
+            return input;
+        }
+    }
+}
