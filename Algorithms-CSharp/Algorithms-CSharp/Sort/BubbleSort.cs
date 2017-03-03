120a44df468b6db1d7e9fb76f5c62e47cd1ac6db
Matt <matt.paauw@gmail.com>
2017-03-02 19:18:33 -0800

    Refactored the BubbleSort.sort() method; added code implementation to BubbleSortTests unit test class, passed all unit tests.

--- Algorithms-CSharp/Algorithms-CSharp/Sort/BubbleSort.cs
+++ Algorithms-CSharp/Algorithms-CSharp/Sort/BubbleSort.cs
@@ -10,22 +10,23 @@ namespace Algorithms_CSharp.Sort
     {
         public int[] sort(int[] input)
         {
-            int n = input.Length;
-            bool swapped = false;
-            do
+            bool swapped = true;
+            int j = 0;
+            while (swapped)
             {
-                for(int i = 1; i <= n - 1; i++)
+                swapped = false;
+                j++;
+                for(int i = 0; i < input.Length - j; i++)
                 {
-                    if(input[i - 1] > input[i])
+                    if(input[i] > input[i + 1])
                     {
-                        int temp = input[i - 1];
-                        input[i - 1] = input[i];
-                        input[i] = temp;
+                        int temp = input[i];
+                        input[i] = input[i + 1];
+                        input[i + 1] = temp;
                         swapped = true;
                     }
                 }
-                n -= 1;
-            } while (!swapped);
+            }
             return input;
         }
     }
--- Algorithms-CSharp/Algorithms-CSharpTests/Sort/BubbleSortTests.cs
+++ Algorithms-CSharp/Algorithms-CSharpTests/Sort/BubbleSortTests.cs
@@ -5,15 +5,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Algorithms_CSharp.Shared;
 
 namespace Algorithms_CSharp.Sort.Tests
 {
     [TestClass]
     public class BubbleSortTests
     {
+        private const int TEST_BREADTH = 200;
+
+        private const int TEST_DEPTH = 50000;
+
+        private TestEngine testDriver;
+
+        private BubbleSort bubbleSort;
+
+        public BubbleSortTests()
+        {
+            this.testDriver = new TestEngine(TEST_BREADTH, TEST_DEPTH);
+            this.bubbleSort = new BubbleSort();
+        }
+
         [TestMethod]
         public void sortTest()
         {
+            // act / assert
+            int[] actualSort = this.bubbleSort.sort(this.testDriver.elements);
+            Array.Sort(this.testDriver.elements);
+            int[] expectedSort = this.testDriver.elements;
+            for(int i = 0; i < actualSort.Length; i++)
+            {
+                Assert.AreEqual(expectedSort[i], actualSort[i]);
+            }
         }
     }
 }
