46e10270eadadab0fbf566a5b53d7e6b7fa8c8e0
Matt <matt.paauw@gmail.com>
2017-03-02 19:20:13 -0800

    Added comment documentation to the BubbleSort class.

--- Algorithms-CSharp/Algorithms-CSharp/Sort/BubbleSort.cs
+++ Algorithms-CSharp/Algorithms-CSharp/Sort/BubbleSort.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace Algorithms_CSharp.Sort
 {
+    /// <summary>
+    /// Class including code to perform a Bubble Sort.
+    /// Efficiency: O(n^2)
+    /// </summary>
     public class BubbleSort
     {
+        /// <summary>
+        /// Takes an unsorted array and performs a Bubble Sort on it.
+        /// </summary>
+        /// <param name="input">Unsorted array to be processed within the sort.</param>
+        /// <returns>Returns the sorted integer array.</returns>
         public int[] sort(int[] input)
         {
             bool swapped = true;
