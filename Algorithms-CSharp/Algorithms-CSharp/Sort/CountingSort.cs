c4e807204e948a4b192bd078b2b20d15c6243c80
Matt <matt.paauw@gmail.com>
2017-03-06 19:02:57 -0800

    Added CountingSort class to the Sort directory, with code implementation.

---
+++ Algorithms-CSharp/Algorithms-CSharp/Sort/CountingSort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_CSharp.Sort
+{
+    class CountingSort
+    {
+        public int[] sort(int[] input)
+        {
+            int max = input[0], min = input[0];
+            int[] tempArray = input;
+            foreach(int item in tempArray)
+            {
+                if(item > max)
+                {
+                    max = item;
+                }
+                if(item <= min)
+                {
+                    min = item;
+                }
+            }
+            int[] indexArray = new int[(max - min) + 1];
+            foreach (int item in tempArray)
+            {
+                indexArray[item - min]++;
+            }
+            int indexToInsert = 0, currentIndex = 0, finishIndex = indexArray.Length;
+            while(currentIndex != finishIndex)
+            {
+                int instanceCount = indexArray[currentIndex];
+                for(int j = 0; j < instanceCount; j++)
+                {
+                    input[indexToInsert++] = currentIndex + min;
+                }
+                currentIndex++;
+            }
+            return input;
+        }
+    }
+}
