2c293bb184f56d74755593246ec850778104eb98
Matt <matt.paauw@gmail.com>
2017-03-06 19:06:02 -0800

    Added code implementation to CountingSortTests unit test file.

--- Algorithms-CSharp/Algorithms-CSharpTests/Sort/CountingSortTests.cs
+++ Algorithms-CSharp/Algorithms-CSharpTests/Sort/CountingSortTests.cs
@@ -5,15 +5,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Algorithms_CSharp.Shared;
 
 namespace Algorithms_CSharp.Sort.Tests
 {
     [TestClass]
     public class CountingSortTests
     {
+        private const int TEST_BREADTH = 200;
+
+        private const int TEST_DEPTH = 50000;
+
+        private TestEngine testDriver;
+
+        private CountingSort countingSort;
+
+        public CountingSortTests()
+        {
+            this.testDriver = new TestEngine(TEST_BREADTH, TEST_DEPTH);
+            this.countingSort = new CountingSort();
+        }
+
         [TestMethod]
         public void sortTest()
         {
+            // act / assert
+            int[] actualSort = this.countingSort.sort(this.testDriver.elements);
+            Array.Sort(this.testDriver.elements);
+            int[] expectedSort = this.testDriver.elements;
+            for(int i = 0; i < actualSort.Length; i++)
+            {
+                Assert.AreEqual(expectedSort[i], actualSort[i]);
+            }
         }
     }
 }
